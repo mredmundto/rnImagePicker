f8a8ddc74a6e99ef70dbbac1ea0092be3c67cb94
mredmundto <yatlunto@gmail.com>
2016-11-09 17:20:10 +0800

    first commit default

---
+++ android/app/src/main/java/com/rntemplate/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rntemplate;
+
+import com.facebook.react.ReactActivity;
+
+// import com.myapp.imagepicker.*; // import the package
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "rntemplate";
+    }
+
+    // @Override
+    // protected List<ReactPackage> getPackages() {
+    //     return Arrays.<ReactPackage>asList(
+    //         new MainReactPackage(),
+    //         new ImagePickerPackage() // include it in getPackages
+    //     );
+    // }
+}
